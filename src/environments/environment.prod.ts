b338a81ce8586e18b3a791ae78d19baf45145ce2
Dian <dian7875.a@gmail.com>
2025-04-20 19:07:14 -0600

    Deploy URL Api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: 'https://transportes-diaz-production.up.railway.app',
+  API_URL: 'https://transportes-diaz-production-f9d4.up.railway.app',
 };
